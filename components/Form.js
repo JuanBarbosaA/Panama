715e58cc854d212bc6bce3255c2a21249ae336eb
Juan Barbosa <juanbarbosavella@gmail.com>
2025-02-24 19:23:48 -0500

    imagen de servicios agregada

--- components/Form.js
+++ components/Form.js
@@ -3,6 +3,6 @@ export default function Form(){
     return(
         <section>
             <Image alt="Form image" src={"/image8.jpg"} width={1500} height={1500}></Image>
-        </section>
+        </section> 
     )
 }
