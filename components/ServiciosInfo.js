a8fd8c1a0e9cf2a075fddc8640dc78af69eb1d20
Juan Barbosa <juanbarbosavella@gmail.com>
2025-02-24 20:06:59 -0500

    fixed

--- components/ServiciosInfo.js
+++ components/ServiciosInfo.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function ServiciosInfo(){
     return(
         <section>
-            <Image alt="Information image" src={"/servicios-info.jpg"} width={1500} height={1500}></Image>
+            <Image alt="Info image" src={"/servicios-information.jpg"} width={1500} height={1500}></Image>
         </section>
     )
 }
